9179ba399ec7588e780b9f3701fe079df0508046
Caio Veras <caiohalves@gmail.com>
2018-05-08 23:19:44 -0300

    atualizacoes

--- backend/api/bilingCycle/bilingCycleService.js
+++ backend/api/bilingCycle/bilingCycleService.js
@@ -3,4 +3,11 @@ const BillingCycle = require('./bilingCycle')
 //Criação dos métodos http para o billingCycle
 BillingCycle.methods(['get', 'post', 'put', 'delete'])
 
+/* 
+ *roda as validações no update
+ *new => exibe no retorno do put no rest service
+ *runValidators => roda as validações escritas no Schema
+ *  
+ */
+BillingCycle.updateOptions({new: true, runValidators: true})
 module.exports = BillingCycle
--- backend/readme.md
+++ backend/readme.md
@@ -1,7 +1,7 @@
 Para Inicar o servidor:
 
-    -> npm run -dev {Inicia carregando as dependencias listadas no package.json }
-    -> npm run -production
+    -> npm run dev {Inicia carregando as dependencias listadas no package.json }
+    -> npm run production
 
     O servidor inicia em loader.js
 
