8c9229b595e811b526f341640e79c64c7dd6ac47
Alves Veras <caio.alves.veras@accenture.com>
2018-05-10 13:44:56 -0300

    conclusão da seção 7

--- backend/api/bilingCycle/bilingCycleService.js
+++ backend/api/bilingCycle/bilingCycleService.js
@@ -7,7 +7,22 @@ BillingCycle.methods(['get', 'post', 'put', 'delete'])
  *roda as validações no update
  *new => exibe no retorno do put no rest service
  *runValidators => roda as validações escritas no Schema
- *  
  */
 BillingCycle.updateOptions({new: true, runValidators: true})
+
+//utiliza o express
+BillingCycle.route('count', function(req,res,next){
+    //utiliza mongoose, a funcção captura o/os error/s que podem retornar do banco e caso exista, exibe abaixo:
+    BillingCycle.count(function(error, value){
+
+        if(error){
+            res.status(500).json({errors: [error]})
+        }else{
+            res.json({value})
+        }
+    })
+
+})
+
+
 module.exports = BillingCycle
