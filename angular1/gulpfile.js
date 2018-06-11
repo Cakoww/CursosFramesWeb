46ff0bf3a1f9547b090306bdcf1354ecc0db7dab
Caio Veras <caio.alves.veas@accenture.com>
2018-06-11 13:07:54 -0300

    Conclusão da seção 11

--- angular1/gulpfile.js
+++ angular1/gulpfile.js
@@ -1,15 +1,21 @@
 const gulp = require('gulp')
+const fs = require('fs-extra')
 const util = require('gulp-util')
-const sequence = require('run-sequence')
 
-require('./gulpTasks/app')
-require('./gulpTasks/deps')
-require('./gulpTasks/server')
+gulp.task('default', function() {
+    fs.walk('gulpTasks').on('data', function (file) {
+      if((/\.(js)$/i).test(file.path)) {
+        require(file.path)
+      }
+    }).on('end', function () {
+      gulp.start('build')
+    })
+})
 
-gulp.task('default', () => {
-    if(util.env.production){
-        sequence('deps', 'app')
-    }else{
-        sequence('deps', 'app', 'server') 
-    }
+gulp.task('build', function() {
+  if(util.env.production) {
+    gulp.start('deps', 'app')
+  } else {
+    gulp.start('deps', 'app', 'server')
+  }
 })
