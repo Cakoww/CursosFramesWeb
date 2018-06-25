095b2a3da0a927fe9c4aba99ea697854d8d0894f
Caio Veras <caio.alves.veas@accenture.com>
2018-06-25 17:48:30 -0300

    commit no trabalho

--- backend/config/server.js
+++ backend/config/server.js
@@ -4,7 +4,7 @@ const port = 3003
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
-const allowCors = require('./cors')
+const allowCors = require('./cors') 
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
